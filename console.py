d540351bb4f8ec3f57380d627af30e08c5495f2f
Raulj06 <5015@holbertonschool.com>
2022-11-28 01:33:13 +0000

    update

--- console.py
+++ console.py
@@ -19,16 +19,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
-              }
+        'BaseModel': BaseModel(), 'User': User(), 'Place': Place(),
+        'State': State(), 'City': City(), 'Amenity': Amenity(),
+        'Review': Review()
+    }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
-             'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
-            }
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
 
     def preloop(self):
         """Prints if isatty is false"""
@@ -37,7 +37,6 @@ class HBNBCommand(cmd.Cmd):
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
-
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
         (Brackets denote optional fields in usage example.)
         """
@@ -73,7 +72,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,15 +113,29 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
+        """ Create an object of any class,
+        cmd_arg = 	Splits the string at the specified separator, and returns a list
+        """
+        cmd_arg = args.split(" ")
+        if not cmd_arg:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        if cmd_arg[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        parts_strings = eval("{}()".format(cmd_arg[0]))
+        for elements in cmd_arg[1:]:
+            """starts at first argument with the command name"""
+            val = elements.split('=')
+            """splits argument as key and value"""
+            val = val[1:-1].replace('_', ' ')
+            setattr(parts_strings, val[0], eval(val[1]))
+
+        new_instance = HBNBCommand.classes[args]
+        new_instance.save()
         storage.save()
+        parts_strings.save()
+        print("{}".format(parts_strings.id))
         print(new_instance.id)
         storage.save()
 
@@ -187,7 +200,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -320,5 +333,7 @@ class HBNBCommand(cmd.Cmd):
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
 
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
+
